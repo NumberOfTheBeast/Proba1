2ebd21d78267eda7d76814296fc9b47e059bd44d
Nadia Georgieva <hope78bg@yahoo.com>
2016-03-21 19:55:53 +0200

    Fixed whitespace

--- SumNumbersFromOneToN.java
+++ SumNumbersFromOneToN.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class SumNumbersFromOneToN {
     public static void main(String[] args) {
-        Scanner numberOfSums= new Scanner(System.in);
-        int numbers= numberOfSums.nextInt();
+        Scanner numberOfSums = new Scanner(System.in);
+        int numbers = numberOfSums.nextInt();
 
-        long sums=0;
+        long sums = 0;
 
         for (int i = 1; i <= numbers ; i++) {
-            sums=sums+i;
+            sums = sums + i;
         }
 
         System.out.println(sums);
